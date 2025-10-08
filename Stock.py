f075b86c0a505981f4ff2352d7f69d3f47bd4a4f
Paulo Villalobos Aravena <kronokechai@gmail.com>
2025-10-08 12:36:19 -0300

    Agregando códigos originales.

---
+++ Stock.py
@@ -0,0 +1,21 @@
+from Ingrediente import Ingrediente
+
+class Stock:
+    def __init__(self):
+        self.lista_ingredientes = []
+
+    def agregar_ingrediente(self, ingrediente):
+        pass
+
+    def eliminar_ingrediente(self, nombre_ingrediente):
+        pass    
+
+    def verificar_stock(self):
+        pass
+
+    def actualizar_stock(self, nombre_ingrediente, nueva_cantidad):
+        pass
+
+    def obtener_elementos_menu(self):
+        pass
+
