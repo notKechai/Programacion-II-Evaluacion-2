386ca3e9ad3bca8d9f6adc24ad31a94e10484e06
Paulo Villalobos Aravena <kronokechai@gmail.com>
2025-10-09 17:22:04 -0300

    feat: Se agrego el menu completo.

--- Menu_catalog.py
+++ Menu_catalog.py
@@ -25,4 +25,52 @@ def get_default_menus() -> List[IMenu]:
             precio=1100,  # ← COMA AQUÍ
             icono_path="IMG/icono_cola_64x64.png",
         ),
+        CrearMenu(
+            "Papas fritas",
+            [
+                Ingrediente("Papas", "kg", 5)
+            ],
+            precio=1100,
+            icono_path="IMG/icono_papas_fritas_64x64.png",
+        ),
+        CrearMenu(
+            "Hamburguesa",
+            [
+                Ingrediente("Pan de hamburguesa", "unid", 1),
+                Ingrediente("Lamina de queso", "unid", 1),
+                Ingrediente("Hamburgesa de carne", "unid", 1)
+            ],
+            precio=3500,
+            icono_path="IMG/icono_hamburguesa_negra_64x64.png",
+        ),
+        CrearMenu(
+            "Panqueques",
+            [
+                Ingrediente("Panqueque", "unid", 2),
+                Ingrediente("Manjar", "kg", 1),
+                Ingrediente("Azucar", "kg", 1)
+            ],
+            precio=2000,
+            icono_path="IMG/icono_panqueques_64x64.png"
+        ),
+        CrearMenu(
+            "Pollo frito",
+            [
+                Ingrediente("Presa de pollo", "unid", 1),
+                Ingrediente("Porcion de harina","unid", 1),
+                Ingrediente("Porcion de aceite", "unid", 1),
+            ],
+            precio=2800,
+            icono_path="IMG/icono_pollo_frito_64x64.png",
+        ),
+        CrearMenu(
+            "Ensalada Mixta",
+            [
+                Ingrediente("Lechuga", "unid", 1),
+                Ingrediente("Tomate", "kg", 1),
+                Ingrediente("Zanahoria rallada", "kg", 1),
+            ],
+            precio=1500,
+            icono_path="IMG/icono_ensalada_mixta_64x64.png"
+        )
     ]
